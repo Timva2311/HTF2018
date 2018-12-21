72e42d4cbeb13082de333fad47f0562799f81b34
Tim Van Aelst <tim.vanaelst@involved-it.be>
2018-12-21 14:08:19 +0100

    Initial commit

---
+++ HTF2018/HTF2018/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTF2018.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
